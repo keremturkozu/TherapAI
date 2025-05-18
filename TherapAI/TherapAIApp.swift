09034a641fa02584c0f25ae319941e4c00a07fb8
Kerem Türközü <turkozukerem@gmail.com>
2025-05-19 01:36:17 +0300

    Initial Commit

---
+++ TherapAI/TherapAIApp.swift
@@ -0,0 +1,32 @@
+//
+//  TherapAIApp.swift
+//  TherapAI
+//
+//  Created by Kerem Türközü on 19.05.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TherapAIApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
